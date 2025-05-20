896d8983232abd7b8c571ce0563e133772e1cf61
Eugene <sshabrovivivi@gmail.com>
2025-05-20 19:34:38 +0400

    class changing, review part is done

--- js/main.js
+++ js/main.js
@@ -1,38 +1,30 @@
 export function manageFlexMain() {
-    console.log(getComputedStyle(document.querySelector(".testimonial__score")).bottom)
-    const container = document.querySelector('.main');
-    const items = Array.from(document.querySelectorAll('.testimonial'));
-   //gap
-   //items display
-   //доступное место
-   //считаем всего места, скрываем если много
-   //запуск при загрузке
-   const gap = parseInt(getComputedStyle(container).gap) || 0;
-  
-   let avaibleHeight = container.offsetHeight;
+  console.log(
+    getComputedStyle(document.querySelector(".testimonial__score")).bottom,
+  );
+  const container = document.querySelector(".main");
+  const items = Array.from(document.querySelectorAll(".testimonial"));
 
-   items.forEach(item => item.style.display = "block");
-   
-   let usedHeight = 0; 
-   
-   
+  const gap = parseInt(getComputedStyle(container).gap) || 0;
 
-  items.forEach((item,index)=>{
-     let totalHeight = item.offsetHeight + (index > 0 ? gap:0);
-      
-     if(totalHeight + usedHeight > avaibleHeight)
-      {
-        item.style.display="none";
-      }else{
-        usedHeight+=totalHeight; 
-      }
-      });
-      
-  window.addEventListener("load",manageFlexMain);
-  window.addEventListener("resize",manageFlexMain);
+  let avaibleHeight = container.offsetHeight;
+
+  items.forEach((item) => (item.style.display = "block"));
+
+  let usedHeight = 0;
+
+  items.forEach((item, index) => {
+    let totalHeight = item.offsetHeight + (index > 0 ? gap : 0);
+
+    if (totalHeight + usedHeight > avaibleHeight) {
+      item.style.display = "none";
+    } else {
+      usedHeight += totalHeight;
+    }
+  });
+
+  window.addEventListener("load", manageFlexMain);
+  window.addEventListener("resize", manageFlexMain);
 }
 
-
-export function fillMainReviewBox() {
-
-}
+export function fillMainReviewBox() {}
--- js/review-panel filler.js
+++ js/review-panel filler.js
@@ -1,96 +1,80 @@
-export function fillReviewPanel(){
-  
-   
-   fetch('/data/product-reviews.json')
-  .then(response => response.json())
-  .then(data => {
-    
-    
-    let [avgRating,amountOfStars,star_1,star_2,star_3,star_4,star_5] = starRating(data);
+export function fillReviewPanel() {
+  fetch('/data/product-reviews.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let [avgRating, amountOfStars, ...starArr] = starRating(data);
 
-        fillReviewPanel__header(avgRating);
+      fillReviewPanel__header(avgRating);
+      fillReviewPanel__score(amountOfStars, starArr);
     });
-  
-  function starRating(data){
-    let sumRating = 0; 
-    let countAmount =0;
+
+  function starRating(data) {
+    let sumRating = 0;
+    let countAmount = 0;
     let star_1 = 0;
     let star_2 = 0;
     let star_3 = 0;
     let star_4 = 0;
     let star_5 = 0;
 
-    for(let d of Object.values(data) ){
-        countAmount++;
-        sumRating+=d.rating;
-        
-        switch(d.rating){
-            case 1:
-                star_1++;
-            break;
-            case 2:
-                star_2++;
-            break;
-            case 3:
-                star_3++;
-            break;
-            case 4:
-                star_4++;
-            break;
-            case 5:
-                star_5++;
-            break;
-        }
+    for (let d of Object.values(data)) {
+      countAmount++;
+      sumRating += d.rating;
+
+      switch (d.rating) {
+        case 1:
+          star_1++;
+          break;
+        case 2:
+          star_2++;
+          break;
+        case 3:
+          star_3++;
+          break;
+        case 4:
+          star_4++;
+          break;
+        case 5:
+          star_5++;
+          break;
+      }
     }
 
-    let avg = Math.floor(sumRating/countAmount * 10) / 10 ;
+    let avg = Math.floor((sumRating / countAmount) * 10) / 10;
 
-    console.log(avg,countAmount,star_1,star_2,star_3,star_4,star_5);
-    return [avg,countAmount,star_1,star_2,star_3,star_4,star_5];
+    console.log(avg, countAmount, star_1, star_2, star_3, star_4, star_5);
+    return [avg, countAmount, star_1, star_2, star_3, star_4, star_5];
   }
 
-  function fillReviewPanel__header(avg){
+  function fillReviewPanel__header(avg) {
+    const ReviewPanel__header = document.querySelector('.review-panel__header');
 
-    const ReviewPanel__header = document.querySelector(".review-panel__header");
-    
-    const Rating = ReviewPanel__header.querySelector(".review-panel__p");
+    const Rating = ReviewPanel__header.querySelector('.js-overall-rating');
+
     Rating.innerHTML = avg;
 
-    const StarsSvg =  ReviewPanel__header.querySelector(".svgPic-starBig");
-    let stars = Array.from( StarsSvg.getElementsByTagName("path") );
-    
-    let starIndex = Math.floor(avg);
-    let part = parseInt( avg.toString().slice(2) ) ; 
-   
-    stars.forEach( (item,index)=>{
-        if( index+1 <= starIndex )
-        item.style.fill="url(#full)";
-        else {
-            switch(part){
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    item.style.fill="url(#third)";
-                break;
-                case 5:
-                    item.style.fill="url(#half)";
-                break;
-                case 6:
-                case 7:
-                case 8:
-                    item.style.fill="url(#two-thirds)";
-                break;
-            }
-        }
-    })
+    const cover = ReviewPanel__header.querySelector('.review-panel__cover');
 
-    // stars[0].style.fill = "currentColor";
-   
-
-
-    // "#ffd800" url(#half)
+    let percent = 100 - (avg / 5) * 100 + '%';
+    cover.style.width = percent;
   }
 
-  
+  function fillReviewPanel__score(amountOfStars, starArr) {
+    const ReviewPanel__score = document.querySelector('.review-panel__score');
+    const starsPercentArr = ReviewPanel__score.querySelectorAll('.js-stars-percent');
+    const ReviewPanel__Covers = ReviewPanel__score.querySelectorAll('.review-panel__cover');
+    const ReviewPanel__rateLines = ReviewPanel__score.querySelectorAll('.review-panel__rate-line');
+
+    let percent = [];
+
+    starsPercentArr.forEach((item, index) => {
+      percent.push(Math.floor((starArr[4 - index] / amountOfStars) * 100));
+      item.innerHTML = percent[index] + '%';
+    });
+
+    ReviewPanel__Covers.forEach((item, index) => {
+      if (percent[index] < 1) item.style.borderRadius = '25px';
+      item.style.width = 100 - percent[index] + '%';
+    });
+  }
 }
